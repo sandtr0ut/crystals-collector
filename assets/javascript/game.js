7a19a87aa3cd92be0639121aff49f10b55d26236
sandtr0ut <47204349+sandtr0ut@users.noreply.github.com>
2019-03-16 14:36:43 -0700

    almost there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,16 @@ $(document).ready(function () {
   var score = 0;
   var wins = 0;
   var losses = 0;
-
+  var targetScore = 0;
 
 
   //create a function that will reset the target score when called
-  function targetScore() {
+function resetGame() {
     //generate a random number between 19 and 120
-    var num = Math.floor(Math.random() * 102 + 19);
-    $("#targetNum").html(num);
-    console.log(num);
-  }
+    targetScore = Math.floor(Math.random() * 102 + 19);
+    $("#targetNum").html(targetScore);
+    console.log(targetScore);
+ 
   
 
   //select all of the crystal values
@@ -22,7 +22,7 @@ $(document).ready(function () {
   //create a function that will reset/reassign the new attribute crystalValues when called
   // function resetGame() {
     //and generate a new target score
-    targetScore();
+   
 
     //loop through the resulting array
     for (var i = 0; i < crystals.length; i++) {
@@ -30,15 +30,15 @@ $(document).ready(function () {
       //gen a random num 1-12 for each iteration
       var randomNum = Math.floor(Math.random() * 12 + 1);
       //create local variable for the crystal at current location [i]
-       var currentElement = crystals[i];
+      //  var currentElement = crystals[i];
 
       //assign the random numbers we just generated to the respective
       //image buttons
-      crystals.data("crystalValue", randomNum);
+      $(crystals[i]).data("crystalValue", randomNum);
       
-      console.log(crystals.data("crystalValue"));
+      
     }
-  // }
+  }
   
   
   //handle clicks on each of the buttons
@@ -46,19 +46,20 @@ $(document).ready(function () {
     score += $(this).data("crystalValue");
     scoreNum.html(score);
     console.log($(this).data("crystalValue"));
-    
+    console.log(score);
+    console.log(targetScore);
     //check if score has exceeded target (loss)
     if (score > targetScore) {
       losses++;
       $("#losses").html(losses);
-      // resetGame();
+      resetGame();
     }
     else if (score == targetScore) {
       wins++;
       $("#wins").html(wins);
-      // resetGame();
+      resetGame();
     }
     
   })
-
+resetGame();
 });

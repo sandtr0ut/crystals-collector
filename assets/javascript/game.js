bc15f9e24d0dec5fd7a656338187af2222b052f2
sandtr0ut <crosbyverse@gmail.com>
2019-06-20 14:36:09 -0700

    fix variable-scope issue, refactor

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,60 @@
 $(document).ready(function () {
 
-  var scoreNum = $("#totalScoreNum");
-  var score = 0;
-  var wins = 0;
-  var losses = 0;
-  var targetScore = 0;
+    var scoreNum = $("#totalScoreNum");
+    var score = 0;
+    var wins = 0;
+    var losses = 0;
+    var targetScore = 0;
+    var crystals = $(".imageButton");
+    var tt = $("#titleText");
 
 
-  //create a function that will reset the target score when called
-function resetGame() {
-    //generate a random number between 19 and 120
-    targetScore = Math.floor(Math.random() * 102 + 19);
-    $("#targetNum").html(targetScore);
-    console.log(targetScore);
- 
-  
+    //create a function that will reset the target score when called
+    function resetGame() {
 
-  //select all of the crystal values
-  var crystals = $(".imageButton");
+        //Generate target score
+        targetScore = Math.floor(Math.random() * 102 + 19);
+        $("#targetNum").html(targetScore);
+        console.log(targetScore);
 
-  //create a function that will reset/reassign the new attribute crystalValues when called
-  // function resetGame() {
-    //and generate a new target score
-   
+        //Update title
+        tt.text("CrystalsCollector!");
 
-    //loop through the resulting array
-    for (var i = 0; i < crystals.length; i++) {
+        //Reset score
+        score = 0;
+        scoreNum.html(score);
 
-      //gen a random num 1-12 for each iteration
-      var randomNum = Math.floor(Math.random() * 12 + 1);
-      //create local variable for the crystal at current location [i]
-      //  var currentElement = crystals[i];
+        //Assign new crystal values
+        for (var i = 0; i < crystals.length; i++) {
 
-      //assign the random numbers we just generated to the respective
-      //image buttons
-      $(crystals[i]).data("crystalValue", randomNum);
-      
-      
+            //gen a random num between one and 12
+            var randomNum = Math.floor(Math.random() * 12 + 1);
+
+            $(crystals[i]).data("crystalValue", randomNum);
+        }
     }
-  }
-  
-  
-  //handle clicks on each of the buttons
-  crystals.on('click', function() {
-    score += $(this).data("crystalValue");
-    scoreNum.html(score);
-    console.log($(this).data("crystalValue"));
-    console.log(score);
-    console.log(targetScore);
-    //check if score has exceeded target (loss)
-    if (score > targetScore) {
-      losses++;
-      $("#losses").html(losses);
-      resetGame();
-    }
-    else if (score == targetScore) {
-      wins++;
-      $("#wins").html(wins);
-      resetGame();
-    }
-    
-  })
-resetGame();
+
+
+    //handle clicks on each of the buttons
+    crystals.on('click', function () {
+        score += $(this).data("crystalValue");
+        scoreNum.html(score);
+        console.log($(this).data("crystalValue"));
+        console.log(score);
+        console.log(targetScore);
+        //check if score has exceeded/met target
+        if (score > targetScore) {
+            tt.html("YOU LOSE!")
+            losses++;
+            $("#losses").html(losses);
+            setTimeout(resetGame, 3000);
+        } else if (score == targetScore) {
+            tt.html("WINNER!")
+            wins++;
+            $("#wins").html(wins);
+            setTimeout(resetGame, 3000);
+        }
+
+    })
+    resetGame();
 });
